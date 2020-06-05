ca1fc3bbec9a4ac9b12d4581185f6ffc0ed83b8c
aritrog <aritraghosh084@gmail.com>
2020-06-05 20:20:20 +0530

    updating the contact route

--- mainapp/routes.py
+++ mainapp/routes.py
@@ -1,6 +1,6 @@
 from flask import url_for,request,render_template,redirect,flash
 from mainapp.cruds import Admissiondb
-from mainapp.forms import AdmissionForm
+from mainapp.forms import AdmissionForm,ContactForm
 from mainapp import app
 @app.route('/')
 def home():
@@ -12,6 +12,8 @@ def contact():
 	form=ContactForm(request.form)
 	if form.validate_on_submit():
 		flash("Your message has been send to the authorities concern!")
+		return redirect(url_for('home'))
+	return render_template('contact.html')	
 
 @app.route('/about',methods=['GET','POST'])
 def about():
@@ -20,11 +22,8 @@ def about():
 @app.route('/admission', methods=['GET','POST'])
 def admission():
 	form=AdmissionForm(request.form)
-	chc=form.validate_on_submit()
-	print(chc)
-	if chc:
-		print("submitted")
-		flash(f"{form.name.data} have been registered in opur database","success")
+	if form.validate_on_submit():
+		flash(f"{form.name.data} have been registered in our database","success")
 		return redirect(url_for('home'))
 	print(form.errors)	
 	return render_template('admission.html', form=form)
