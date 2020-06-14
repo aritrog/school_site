7f5fb1345e87d0985c7434d97e26a2cc033d92ed
debapam11 <debapam11@gmail.com>
2020-06-14 23:33:13 +0530

    multi database added

--- mainapp/cruds.py
+++ mainapp/cruds.py
@@ -73,7 +73,8 @@ class Admissiondb(db.Model):
 		return f"user('{self.name}','{self.email}','{self.phno}','{self.age}','{self.classval}','{self.address}','{self.date}')"	
 
 class LogUser(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100),nullable=False)
-    name = db.Column(db.String(1000),nullable=False)
+	__bind_key__ = 'login'
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(100), unique=True)
+	password = db.Column(db.String(100),nullable=False)
+	name = db.Column(db.String(1000),nullable=False)
